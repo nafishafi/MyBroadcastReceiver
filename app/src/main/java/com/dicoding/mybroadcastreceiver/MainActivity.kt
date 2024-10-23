b6622804d3e149a7cf72030cd65a73817bdfb063
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-10-23 11:07:08 +0700

    add Req Permission

--- app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.kt
@@ -2,11 +2,14 @@ package com.dicoding.mybroadcastreceiver
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.dicoding.mybroadcastreceiver.databinding.ActivityMainBinding
+import android.Manifest
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -20,10 +23,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding?.btnPermission?.setOnClickListener(this)
 
     }
+    var requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onClick(v: View?) {
         when (v?.id) {
-
+            R.id.btn_permission -> requestPermissionLauncher.launch(Manifest.permission.RECEIVE_SMS)
         }
     }
 
